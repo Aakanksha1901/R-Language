7cd687276cb3cdcd298b7f04f5442ece0eb7925a
Aakanksha1901 <124653368+Aakanksha1901@users.noreply.github.com>
2026-03-14 10:06:58 +0530

    Add files via upload

---
+++ Adding 5 to each element.R
@@ -0,0 +1,7 @@
+#Add 5 to Vector
+#Add 5 to each element of vector.
+
+
+Vec1 <- c(20,30,40,50)
+Vec1 + 5
+print(Vec1)
---
+++ Addition of Matrix.R
@@ -0,0 +1,10 @@
+#Matrix Addition
+#Add two matrices.
+
+
+M1 <- matrix (c(1, 2, 3, 4), nrow = 2 , ncol= 2)
+
+M2 <- matrix (c(5, 6, 7, 8), nrow =2 , byrow = TRUE)
+Addition <- M1 + M2
+print(Addition)
+
